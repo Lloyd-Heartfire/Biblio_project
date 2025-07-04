80a5d8cfd0749b4413f10c9214a4fbd6f5b7d0b6
Lloyd Heartfire <beaudonloic@hotmail.com>
2025-07-04 10:24:01 +0200

    Add repositories

---
+++ back/app/repositories/author-repository.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Session
+from models.author import Author
+from .base import BaseRepository
+
+class AuthorRepository(BaseRepository):
+    def __init__(self, db: Session):
+        super().__init__(db, Author)
+
+    def get_by_name(self, name: str):
+        return self.db.query(self.model).filter_by(name=name).first()
